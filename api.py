f425690eb615c822cc64b63429a1ce6b200a7bad
prasanth <massprsanth959@gmail.com>
2023-09-14 00:04:46 +0530

    download method added,upload  progress design

--- api.py
+++ api.py
@@ -1,8 +1,10 @@
 import os
+import io
 import mimetypes
 from pprint import pprint
+import tqdm
 from handler import Create_Service
-from googleapiclient.http import MediaFileUpload
+from googleapiclient.http import MediaFileUpload, MediaIoBaseDownload
 
 
 CRENDS_FILE = "/data/data/com.termux/files/home/newproject/credentials.json"
@@ -15,6 +17,12 @@ SCOPES = ["https://www.googleapis.com/auth/drive"]
 
 root_id = "1GyWgVKNpDB6Pjm6a4HNC5PTmh4byS_IR"
 
+SAVE_PATH = os.environ.get("HOME") + "/gdriveFiles"
+print(SAVE_PATH)
+
+if not os.path.exists(SAVE_PATH):
+    os.mkdir(SAVE_PATH)
+
 
 # Convert bytes to mega bytes
 
@@ -26,20 +34,34 @@ def convert_bytes_to_megabytes(data_size):
 def convert_bytes_to_gigabytes(data_size):
     return data_size / (1024**3)
 
-def clear() :
-  clear_cmd =  'cls' if os.name == 'nt' else 'clear'
-  os.system(clear_cmd)
 
-def picInfo(percent) :
-    approx_percent = str(percent).split('.')[0]
+def clear():
+    clear_cmd = "cls" if os.name == "nt" else "clear"
+    os.system(clear_cmd)
 
-    progress = ["[□  □ □ □ □ □ □ □ □ □]","[■ □ □ □ □ □ □ □ □ □]","[■ ■ □ □ □ □ □ □ □ □]", "[■ ■ ■ □ □ □ □ □ □ □]", "[■ ■ ■ ■ □ □ □ □ □ □]", "[■ ■ ■ ■ ■ □ □ □ □ □]", "[■ ■ ■ ■ ■ ■ □ □ □ □]", "[■ ■ ■ ■ ■ ■ ■ □ □ □]", "[■ ■ ■ ■ ■ ■ ■ ■ □ □]", "[■ ■ ■ ■ ■ ■ ■ ■ ■ □]", "[■ ■ ■ ■ ■ ■ ■ ■ ■ ■]"] 
 
-    if len(approx_percent) == 1 :
+def picInfo(percent):
+    approx_percent = str(percent).split(".")[0]
+
+    progress = [
+        "[□  □ □ □ □ □ □ □ □ □]",
+        "[■ □ □ □ □ □ □ □ □ □]",
+        "[■ ■ □ □ □ □ □ □ □ □]",
+        "[■ ■ ■ □ □ □ □ □ □ □]",
+        "[■ ■ ■ ■ □ □ □ □ □ □]",
+        "[■ ■ ■ ■ ■ □ □ □ □ □]",
+        "[■ ■ ■ ■ ■ ■ □ □ □ □]",
+        "[■ ■ ■ ■ ■ ■ ■ □ □ □]",
+        "[■ ■ ■ ■ ■ ■ ■ ■ □ □]",
+        "[■ ■ ■ ■ ■ ■ ■ ■ ■ □]",
+        "[■ ■ ■ ■ ■ ■ ■ ■ ■ ■]",
+    ]
+
+    if len(approx_percent) == 1:
         return progress[0] + "  {:.2f} %".format(percent)
-    if len(approx_percent) == 2 :
+    if len(approx_percent) == 2:
         return progress[int(approx_percent[0])] + "  {:.2f} %".format(percent)
-    if len(approx_percent) == 3 :
+    if len(approx_percent) == 3:
         return progress[-1] + "  {:.2f} %".format(percent)
 
 
@@ -95,7 +117,7 @@ def getAccountInfo():
     accInfo = service.about().get(fields="*").execute()
 
     storageQuota = accInfo["storageQuota"]
-    user = accInfo['user']
+    user = accInfo["user"]
 
     used = convert_bytes_to_gigabytes(int(storageQuota["usage"]))
 
@@ -111,7 +133,7 @@ def getAccountInfo():
     print(f"Owner : {user['displayName']}")
     print(f"Email Address : {user['emailAddress']}\n")
     print("$$$$STORAGE DETAILS$$$$\n")
-    print(picInfo((used/limit) * 100) + "\n")
+    print(picInfo((used / limit) * 100) + "\n")
     print("Used : {:.2f}GB of {}GB".format(used, limit))
     print("Free : {:.2f}GB of {}GB".format(free, limit))
     print("Trash : {:.2f}GB of {:.2f}GB\n".format(trashUsedInDrive, used))
@@ -136,9 +158,10 @@ def uploadFile(path, rootFolderId=None):
     media = MediaFileUpload(path, mimetype=mimetype, resumable=True)
     gfile = (
         service.files()
-        .create(body=fileMetadata, media_body=media, fields="id,name").execute()
-        )
-    
+        .create(body=fileMetadata, media_body=media, fields="id,name")
+        .execute()
+    )
+
     print(f"{gfile['name']} uploaded successfully")
     return
 
@@ -219,3 +242,57 @@ def cloneFolder(real_file_id, root_folder_id=None):
 
 
 # print("{} folders {} files cloned successfully".format(folders, files))
+
+
+def downloadFile(realFileId: str, savePath: str = SAVE_PATH):
+    service = Create_Service(CRENDS_FILE, API_NAME, API_VERSION, SCOPES)
+
+    file_metadata = service.files().get(fileId=realFileId, fields="id,name").execute()
+
+    request = service.files().get_media(fileId=realFileId)
+
+    fh = io.BytesIO()
+
+    downloader = MediaIoBaseDownload(fd=fh, request=request)
+
+    response = None
+    """
+    with tqdm(
+        downloader,initial=1, desc="File Downloading... "
+    ) as progressBar:
+    """
+    while response is None:
+        status, response = downloader.next_chunk()
+
+        print(status.progress() * 100)
+
+#        progressBar.update(int(status.progress()) * 100)
+
+    with open(os.path.join(savePath, file_metadata["name"]), "wb") as file:
+        file.write(fh.getvalue())
+
+    print(f"{file_metadata['name']} downloaded successfully")
+
+
+
+def downloadFolder(realFileId: str,savePath: str = SAVE_PATH) :
+    service = Create_Service(CRENDS_FILE, API_NAME, API_VERSION, SCOPES)
+    
+    fileMetadata = service.files().get(fileId=realFileId,fields='id,name').execute()
+
+    query = f"{realFileId} in parents"
+    getResults = search(service,query)
+
+    cur_path = os.path.join(savePath,fileMetadata['name'])
+    if not os.path.exists(cur_path) :
+        os.mkdir(cur_path)
+
+    for file in getResults :
+        if file[2] == "application/vnd.google-apps.folder" :
+            downloadFolder(file[0],cur_path)
+        else :
+            downloadFile(file[0],cur_path)
+
+
+
+
